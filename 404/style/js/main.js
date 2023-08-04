ca3792cf508574142ed0a86b7387126412f3460e
Ahmad Ilham Kushartanto <63383454+work-ham@users.noreply.github.com>
2023-08-04 09:54:24 +0700

    Add files via upload

---
+++ 404/style/js/main.js
@@ -0,0 +1,16 @@
+// return a random number within a range
+function random(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+// map a number from 1 range to another
+function map(n, start1, end1, start2, end2) {
+  return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
+}
+
+
+document
+  .querySelector(".overlay__btn--colors")
+  .addEventListener("click", () => {
+   
+  });
